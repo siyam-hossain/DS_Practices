57c0442fa9bc0f0bbdcb754f5997371bd3206b2c
Siyam Hossain <siyam.cybersoul@gmail.com>
2024-12-29 20:41:23 +0600

    Create 1_how_c_node.cpp

---
+++ 1_how_c_node.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//This is the node
+class node
+{
+    public:
+    
+    int value;
+    node* next;
+    
+    node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
