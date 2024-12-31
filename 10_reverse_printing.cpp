f8561e7b2cc9015e6206113f696a8e1886f0ff58
Siyam Hossain <siyam.cybersoul@gmail.com>
2024-12-31 23:35:00 +0600

    Create 10_reverse_printing.cpp

---
+++ 10_reverse_printing.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//This is the node
+class node
+{
+    public:
+    
+    int value;
+    node* next;
+    
+    node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void in_at_tail(node* &head, node* &tail, int value);
+void reverse(node* temp);
+
+
+int main(int argc, char const *argv[])
+{
+    node* head = NULL;
+    node* tail = NULL;
+
+
+    int value;
+    while (cin>>value && value != -1)
+    {
+        in_at_tail(head,tail,value);
+    }
+
+    reverse(head);
+    
+    
+    return 0;
+}
+
+void in_at_tail(node* &head, node* &tail, int value)
+{
+    node* newNode = new node(value);
+
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+
+void reverse(node* temp)
+{
+    if(temp == NULL)
+    {
+        return;
+    }
+    reverse(temp->next);
+    cout<<temp->value<<" ";
+}
